fb88908815d60c245df24be68826a61404d3c7af
anshjain16 <anshjain0216@gmail.com>
2024-05-15 18:53:41 +0530

    learning templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(Request):
-    return HttpResponse('Home Page')
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
 
-def room(Request):
-    return HttpResponse('Rooms')
+def home(Request):
+    context = {'rooms': rooms}
+    return render(Request, 'base/home.html', context)
+
+def room(Request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {'room': room}
+    return render(Request, 'base/room.html', context)
